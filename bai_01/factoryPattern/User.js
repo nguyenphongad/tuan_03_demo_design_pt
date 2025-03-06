31d87ef9f137840f3fad983b01b83822251e9a79
PHONG <cuphong2003hk@gmail.com>
2025-03-06 08:19:33 +0700

    create User factoryPattern bai 01

---
+++ bai_01/factoryPattern/User.js
@@ -0,0 +1,23 @@
+class User {
+    constructor(name, roleUser) {
+        this.name = name;
+        this.roleUser = roleUser
+    }
+    getInformation() {
+        return "name : " + this.name + "| role :" + this.roleUser;
+    }
+}
+
+class CreateUserFactory {
+    static createUser(name, roleUser){
+        switch (roleUser) {
+            case "member":
+                return new User(name, "MEMBER");
+            case "admin":
+                return new User(name, "ADMIN");
+            default:
+                throw console.error("role khong hop le");
+                
+        }
+    }
+}
